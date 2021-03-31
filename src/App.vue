d6cd5fd96f3d2047e92a4d35819fbc725c8cccd3
Marko Korhonen <marko.korhonen@druid.fi>
2021-03-31 17:41:57 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="BloxCar logo" class="logo" src="./assets/bloxcar_brand_logo_red.svg">
+  <HomePage msg="Welcome to BloxCar"/>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.logo {
+  max-width: 50%;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Location></Location>
+  </div>
+</template>
+
+<script>
+import Location from './Location.vue';
+
+export default {
+  name: 'HomePage',
+  components: {Location},
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
